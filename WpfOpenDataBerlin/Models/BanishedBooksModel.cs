b11744573026875e66f43e568d48ca7b7c914c2d
BotMaster3000 <jdgebauer@gmx.de>
2018-06-08 14:24:22 +0200

    Added NewtonSoft.Json and Logic to BanishedBooksViewModel
    The Logic from the old project was taken over, since it could still be used in WPF

--- WpfOpenDataBerlin/Models/BanishedBooksModel.cs
+++ WpfOpenDataBerlin/Models/BanishedBooksModel.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace WpfOpenDataBerlin.Models
 {
-    class BanishedBooksModel
+    public class BanishedBooksModel
     {
+        public string AuthorFirstName { get; set; }
+        public string AuthorLastName { get; set; }
+        public string FirstEditionPublicationPlace { get; set; }
+        public string FirstEditionPublicationYear { get; set; }
+        public string FirstEditionPublisher { get; set; }
+        public string Title { get; set; }
+        public string AdditionalInfos { get; set; }
+        public string SecondEditionPublisher { get; set; }
+        public string SecondEditionPublicationPlace { get; set; }
+        public string SecondEditionPublicationYear { get; set; }
+
+        // DIE UNTEREN WERDEN NICHT ANGEZEIGT
+        public string ID { get; set; }
+        public string SsFlag { get; set; }
+        public string Ocrresult { get; set; }
+        public string PageNumberInocrDocument { get; set; }
     }
 }
